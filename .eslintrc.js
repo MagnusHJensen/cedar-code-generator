18c5e34fb2f4ae37e1ec901b9099747267e221d7
Magnus Jensen <magnushjensen.mail@gmail.com>
2024-03-13 21:42:32 +0100

    initial structure

---
+++ .eslintrc.js
@@ -0,0 +1,3 @@
+module.exports = {
+  extends: ['@lego/eslint-config/typescript', '@lego/eslint-config/prettier', '@lego/eslint-config/jest']
+}
---
+++ prettier.config.js
@@ -0,0 +1,1 @@
+module.exports = require("@lego/prettier-config");

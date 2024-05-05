af3530a905e2ff6e96a2eef138fa55cb74fae666
Magnus Jensen <magnushjensen.mail@gmail.com>
2024-05-05 14:12:34 +0200

    fix action prefix method not adding 'Action' to the output (#11)

--- packages/cedar-codegen-core/tests/test-run.test.ts
+++ packages/cedar-codegen-core/tests/test-run.test.ts
@@ -1,13 +1,14 @@
+import { main } from '@cedar-codegen/core';
 import { readFileSync } from 'node:fs';
-import { main } from '../src/index.js';
+import { join } from 'node:path';
 
 test('matches snapshot', () => {
   main({
-    output: `${__dirname}/test-files/cedar-types.ts`,
-    schema: `${__dirname}/test-files/test.cedarschema.json`
+    output: join(__dirname, '/test-files/cedar-types.ts'),
+    schema: join(__dirname, '/test-files/test.cedarschema.json')
   });
 
   expect(
-    readFileSync(`${__dirname}/test-files/cedar-types.ts`).toString()
+    readFileSync(join(__dirname, '/test-files/cedar-types.ts')).toString()
   ).toMatchSnapshot();
 });
